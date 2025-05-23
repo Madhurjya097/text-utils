724cf5ad73f9839006a8e4cff32e4c83734d4277
Madhurjya097 <madhurjyadas097@gmail.com>
2025-05-23 23:50:00 +0530

    Fix Navbar redirect to 404 Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@
 // const app_logo192 = "logo192.png"
 
 export default function Navbar(props) {
+    const public_url = process.env.PUBLIC_URL
     const mode = props.mode
     const capFirstLetter = (str) => {
         let arr = str.split("")
@@ -29,15 +30,15 @@ export default function Navbar(props) {
         <>
             <nav className={`navbar navbar-expand-lg bg-${mode} navbar-${mode}`}>
                 <div className="container-fluid">
-                    <img src={process.env.PUBLIC_URL + "/logo246.png"} alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-2" />
-                    <a className="navbar-brand" href="/">{props.title}</a>
+                    <img src={public_url + "/logo246.png"} alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-2" />
+                    <a className="navbar-brand" href={public_url}>{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a onClick={showActive} className="nav-link active" href="/">Home</a>
+                                <a onClick={showActive} className="nav-link active" href={public_url}>Home</a>
                             </li>
 
                         </ul>
